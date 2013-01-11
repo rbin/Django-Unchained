e87c68b240f553f3160205dd7ef5e18c2f755c3d
Robin <robin@sosour.com>
2013-01-11 11:20:01 +0000

    Do you know what a bounty hunter is?

---
+++ README
@@ -0,0 +1,51 @@
+==== Django Unchained!
+
+Django-Backend support for Couchbase 2.0, utilising CouchbaseKit (Couchbase Python wrapper for Doc Validation)
+
+Because we all know, NoSQL is the future, and some of us want Non-relational data in our Django apps!
+
+Inspired by Django-nonrel, Powered by CouchbaseKit and Couchbase' Python SDK.
+
+
+====== UNFINISHED AND FAR FROM PRODUCTION READY! DO NOT USE YET!
+
+
+==== Couchbase & CouchbaseKit
+
+You can get detailed information about couchbase itself from
+http://www.couchbase.com/ and about its Python driver form
+http://www.couchbase.com/develop/python/next.
+
+Documentation: https://couchbasekit.readthedocs.org/en/latest/
+
+
+Quick Start
+===========
+
+=== UNFINISHED.
+
+
+    from couchbasekit import Connection
+
+    # you should do this in your base settings.py:
+    Connection.auth('myusername', 'p@ssword')
+
+
+Then define your model document.
+
+**models.py**::
+
+	from django.db import models
+	from django.utils.translation import ugettext_lazy as _
+  from couchbasekit import Document
+  from couchbasekit.fields import EmailField, ChoiceField
+
+
+    class Author(Document):
+        __bucket_name__ = 'default'
+        __key_field__ = 'slug' # optional
+        doc_type = 'user'
+        structure = {
+            'slug': unicode,
+            'first_name': unicode,
+            'last_name': unicode,
---
+++ setup.py
@@ -0,0 +1,96 @@
+from couchbasekit import __version__
+from distutils.core import setup
+from distutils.command.install_data import install_data
+from distutils.command.install import INSTALL_SCHEMES
+import os
+import sys
+
+class osx_install_data(install_data):
+
+    def finalize_options(self):
+        self.set_undefined_options('install', ('install_lib', 'install_dir'))
+        install_data.finalize_options(self)
+
+if sys.platform == "darwin": 
+    cmdclasses = {'install_data': osx_install_data} 
+else: 
+    cmdclasses = {'install_data': install_data} 
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+# Tell distutils to put the data_files in platform-specific installation
+# locations. See here for an explanation:
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+django_dir = 'django'
+
+for dirpath, dirnames, filenames in os.walk(django_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+# Small hack for working with bdist_wininst.
+# See http://mail.python.org/pipermail/distutils-sig/2004-August/004134.html
+if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
+    for file_info in data_files:
+        file_info[0] = '\\PURELIB\\%s' % file_info[0]
+
+# Dynamically calculate the version based on django.VERSION.
+version = __import__('django').get_version()
+if u'SVN' in version:
+    version = ' '.join(version.split(' ')[:-1])
+
+setup(
+    name = "Django-Unchained",
+    version = version.replace(' ', '-'),
+    url = 'http://rbin.co',
+    author = 'Robin J',
+    author_email = 'robin@couchbase.com',
+    description = 'A Couchbase backend for Django',
+    download_url = 'http://notyet.com',
+    packages = packages,
+    cmdclass = cmdclasses,
+    data_files = data_files,
+    scripts = ['django/bin/django-admin.py'],
+    classifiers = ['Development Status :: 5 - Production/Stable',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.4',
+                   'Programming Language :: Python :: 2.5',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Topic :: Internet :: WWW/HTTP',
+                   'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+                   'Topic :: Internet :: WWW/HTTP :: WSGI',
+                   'Topic :: Software Development :: Libraries :: Application Frameworks',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   ],
+)
